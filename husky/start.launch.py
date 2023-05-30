77414a7796b93a3363f84baf594293e742402c8f
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2023-05-30 11:24:29 +0200

    Added start script.

---
+++ husky/start.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+def generate_launch_description():
+	return LaunchDescription([
+
+		# Gazebo
+		IncludeLaunchDescription(
+			PythonLaunchDescriptionSource(
+				os.path.join(
+					get_package_share_directory('ros_ign_gazebo'),
+					'launch',
+					'ign_gazebo.launch.py'
+				)
+			),
+			launch_arguments={
+				'ign_args': ' -r cave_circuit.sdf',
+				'ign_version': '6'
+			}.items()
+		),
+
+		# IGN-ROS-Bridge
+		IncludeLaunchDescription(
+			PythonLaunchDescriptionSource('ign_bridge.launch.py')
+		)
+	])
+
